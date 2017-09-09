ac8b8dee1065e587be179f9b5e68a9af841c123e
test api11 <testapi11@wavemaker.com>
2017-09-09 14:14:51 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_QZjMGpWlcZ",
+  "homePage" : "Main",
+  "name" : "Auto_QZjMGpWlcZ",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
